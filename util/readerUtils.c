9fe8af016dcb24a59ef46791bbb51ce520203605
Lisaf Iluz <liluz@ebay.com>
2023-01-14 18:05:34 +0200

    Open Project

---
+++ util/readerUtils.c
@@ -0,0 +1,32 @@
+//
+// Created by Iluz, Lisaf on 14/01/2023.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define START_SIZE 80  // Each Assembly sentence has 80 chars
+#define ADD_SIZE 64  // In case we need to get Staring that bigger than 80 chars
+
+
+//todo: 1.understand 2.use other variable names 3. read in the book for alternate solution
+char *readData(FILE *fp) {
+    int size, len;
+    char *str;
+    int ch;
+
+    size = START_SIZE;
+    len = 0;  // todo: understand this type size_t
+    str = realloc(NULL, sizeof(*str) * START_SIZE);  /*size is start size*/
+    // if (!str)return str;  //todo probably for not enough memory, remove it or find it in the book
+    while (EOF != (ch = fgetc(fp)) && ch != '\n') {
+        str[len++] = ch;
+        if (len == size) {
+            str = realloc(str, sizeof(*str) * (size += ADD_SIZE));
+            //if (!str)return str;
+        }
+    }
+    str[len++] = '\0';
+
+    return realloc(str, sizeof(*str) * len);
+}
